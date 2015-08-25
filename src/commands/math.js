46015a7369320dcb2b7be4278d8bb5c499678f32
Matthew Litwin <mlitwin@kcptech.com>
2015-08-25 16:01:44 -0700

    xhtml correctness: Attribute values need to be quoted
    http://dev.w3.org/html5/html-polyglot/html-polyglot.html 4.8 Attributes
    we quote mathquill-command-id and mathquill-block-id values

--- src/commands/math.js
+++ src/commands/math.js
@@ -233,7 +233,7 @@ var MathCommand = P(MathElement, function(_, super_) {
 
     var cmd = this;
     var blocks = cmd.blocks;
-    var cmdId = ' mathquill-command-id=' + cmd.id;
+    var cmdId = ' mathquill-command-id="' + cmd.id + '"';
     var tokens = cmd.htmlTemplate.match(/<[^<>]+>|[^<>]+/g);
 
     pray('no unmatched angle brackets', tokens.join('') === this.htmlTemplate);
@@ -267,7 +267,7 @@ var MathCommand = P(MathElement, function(_, super_) {
       }
     }
     return tokens.join('').replace(/>&(\d+)/g, function($0, $1) {
-      return ' mathquill-block-id=' + blocks[$1].id + '>' + blocks[$1].join('html');
+      return ' mathquill-block-id="' + blocks[$1].id + '">' + blocks[$1].join('html');
     });
   };
 
